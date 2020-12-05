7b640a31d5196864845eaa9f4e223562a85d2d4b
Luke Oleson <luke.oleson@flyt.io>
2020-12-04 16:17:11 -0800

    cleaning up day 3

--- 2020/day-3/main.go
+++ 2020/day-3/main.go
@@ -7,32 +7,21 @@ import (
 
 func main() {
 
-	// Right 1, down 1 -> 66
-	// Right 3, down 1 -> 200
-	// Right 5, down 1 -> 76
-	// Right 7, down 1 -> 81
-	// Right 1, down 2 -> 46
-
-	run := 1
-	rise := 2
-
+	rise, run := 2, 1
 	grid := input.Input
-	fmt.Println("grid length:", len(grid))
 	var trees int
 	var col int
 	cols := len(grid[0])
-	fmt.Println("cols:", cols)
 
-	for i := rise; i < len(grid); i += rise {
+	for row := rise; row < len(grid); row += rise {
 		col += run
 		if col >= cols {
 			col -= cols
 		}
-		row := grid[i]
-		//fmt.Printf("row: %v, col: %v: \n", i, position)
+		row := grid[row]
 		if row[col] == '#' {
 			trees++
 		}
 	}
-	fmt.Println("answer: ", trees)
+	fmt.Printf("Solution for Rise: %v, Run: %v: %v\n", rise, run, trees)
 }
